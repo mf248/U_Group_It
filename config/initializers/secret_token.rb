1a540c56bdb1363e9a035490b32f3ad628bebe75
Michael Fergus <mfergus48@gmail.com>
2012-10-01 00:16:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UGroupIt::Application.config.secret_token = '155b0dfff3473d9952f06af1191ce21f026f87f90dfecfd596e0d2262eccb076d4ee68d925e3ce2c27fb944f63f0a6edabf2337f6f9f6537d118fb625afe42c3'
